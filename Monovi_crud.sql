0136ab67470e43e204bbe944b7e80f6a8e73e7b4
Özlem KILIÇ <122043812+OzlemKlc@users.noreply.github.com>
2023-08-21 15:38:25 +0300

    Create Monovi_crud.sql

---
+++ Monovi_crud.sql
@@ -0,0 +1,64 @@
+SELECT TOP (1000) [Id]
+      ,[Name]
+      ,[Price]
+      ,[Stock]
+  FROM [MonoviDB].[dbo].[Products]
+
+
+  INSERT INTO Products (Name, Price, Stock)
+VALUES
+    ('Product 1', 10.99, 50),
+    ('Product 2', 25.50, 100),
+    ('Product 3', 8.75, 75),
+    ('Product 4', 19.99, 60),
+    ('Product 5', 14.25, 90),
+    ('Product 6', 7.49, 30),
+    ('Product 7', 32.00, 120),
+    ('Product 8', 11.75, 70),
+    ('Product 9', 28.50, 110),
+    ('Product 10', 5.99, 40),
+    ('Product 11', 16.00, 85),
+    ('Product 12', 9.25, 55),
+    ('Product 13', 22.99, 95),
+    ('Product 14', 12.50, 65),
+    ('Product 15', 18.75, 80),
+    ('Product 16', 14.99, 70),
+    ('Product 17', 29.50, 105),
+    ('Product 18', 6.75, 45),
+    ('Product 19', 21.00, 75),
+    ('Product 20', 8.25, 50),
+    ('Product 21', 24.99, 100),
+    ('Product 22', 15.50, 65),
+    ('Product 23', 10.75, 40),
+    ('Product 24', 27.00, 90),
+    ('Product 25', 9.75, 55),
+    ('Product 26', 23.50, 80),
+    ('Product 27', 13.99, 70),
+    ('Product 28', 31.25, 110),
+    ('Product 29', 7.75, 45),
+    ('Product 30', 20.00, 60);
+
+
+
+	INSERT INTO Products (Name, Price, Stock)
+VALUES
+    ('Product 31', 11.99, 70),
+    ('Product 32', 26.50, 120),
+    ('Product 33', 9.75, 90),
+    ('Product 34', 18.99, 55),
+    ('Product 35', 13.25, 100),
+    ('Product 36', 6.49, 80),
+    ('Product 37', 33.00, 150),
+    ('Product 38', 12.75, 65),
+    ('Product 39', 27.50, 110),
+    ('Product 40', 4.99, 30),
+    ('Product 41', 15.00, 85),
+    ('Product 42', 8.25, 60),
+    ('Product 43', 23.99, 95),
+    ('Product 44', 10.50, 70),
+    ('Product 45', 17.75, 80),
+    ('Product 46', 13.99, 90),
+    ('Product 47', 30.50, 100),
+    ('Product 48', 7.75, 40),
+    ('Product 49', 22.00, 75),
+    ('Product 50', 5.25, 50);
